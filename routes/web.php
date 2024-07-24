3094a471029bc205a66d39f12d6e8b329fb68b50
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-07-24 16:20:10 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('posts.index');
+})->name('home');
+Route::view('/register','auth.register')->name('register');
+Route::post ('/register',[AuthController::class,'register']);
+
+
+Route::get('/login',function(){
+    return view('auth.login');
+})->name('login');
+
+
