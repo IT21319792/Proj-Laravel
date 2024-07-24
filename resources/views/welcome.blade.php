49b3ab59f8b026b684b79f292104c0cba8bbb049
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-07-24 13:00:59 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{env('APP_NAME')}}</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
-</head>
-<body>
- 
-    <h1>Welcome to Laravel</h1>
+@extends('layout')
+@section('main')
+  
+<x-layout>
+    <h1 class="text-pretty">"this was passed from section to yeild"</h1>
+</x-layout>
     
-</body>
-</html>
+@endsection
