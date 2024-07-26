738db2e3916a13797d4ace8e3a32d037d03b2bc2
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-07-26 14:35:37 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,7 @@ Route::view('/register','auth.register')->name('register');
 Route::post ('/register',[AuthController::class,'register']);
 
 
-Route::get('/login',function(){
-    return view('auth.login');
-})->name('login');
+Route::view('/login','auth.login')->name('login');
+Route::post ('/login',[AuthController::class,'login']);
 
 
